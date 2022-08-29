9bd75dc8b4a88d259effb94b3d2d68cfe887a7bc
Myk <89474773+mykcryptodev@users.noreply.github.com>
2022-08-29 08:56:30 -0400

    savings help modal

--- dapp/components/swap/Savings.jsx
+++ dapp/components/swap/Savings.jsx
@@ -1,5 +1,5 @@
 import { useContext, useMemo, useEffect, useState } from 'react';
-import { MoreHorizontal } from 'react-feather';
+import { MoreHorizontal, HelpCircle } from 'react-feather';
 import SwapContext from '../../context/SwapContext';
 
 export const Savings = () => {
@@ -35,7 +35,29 @@ export const Savings = () => {
     <div className="w-full flex justify-center">
       { !savings ? <MoreHorizontal className="h-5 w-5" /> :
         <div className="flex flex-col w-full border-opacity-50">
-          <div className="divider">You are saving {buySavings}%!</div>
+          <div className="divider" style={{ gap: 0 }}>
+            <div className="flex items-center mx-2">
+              <span className="mr-1">You are saving {buySavings}%!</span>
+              <label htmlFor="my-modal-4" className="btn btn-circle btn-ghost btn-xs shrink">
+                <HelpCircle className="h-5 w-5" />
+              </label>
+            </div>
+          </div>
+          <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+          <label htmlFor="my-modal-4" className="modal cursor-pointer">
+            <label className="modal-box relative" htmlFor="">
+              <h3 className="text-lg font-bold">PawSwap saves you from fees!</h3>
+              <p className="py-4">
+                PawSwap provides incentives for projects to list with lower fees compared to other DEXs.
+              </p>
+              <p className="pb-2">
+                That means that you are taxed less when swapping on PawSwap!
+              </p>
+              <p className="pb-2">
+                Consider donating a portion of your savings to one of the thousands causes listed on PawSwap!
+              </p>
+            </label>
+          </label>
         </div>
       }
     </div>
