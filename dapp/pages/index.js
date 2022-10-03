18558f7556639dc4134bc2f010159cb3976ecb5e
Myk <89474773+mykcryptodev@users.noreply.github.com>
2022-10-03 18:24:16 -0400

    fix build error

--- dapp/pages/index.js
+++ dapp/pages/index.js
@@ -136,7 +136,7 @@ const Landing = () => {
             </div>
             <div className="h-full w-full shadow-xl rounded-xl bg-gradient-to-tr from-primary via-secondary to-accent p-2">
               <div className="flex h-full w-full relative" style={{ background: 'none transparent' }}>
-                <iframe className="rounded-xl" style={{aspectRatio: 16/9}} width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/5h8FnQBrA-o?rel=0&amp;autoplay=1&amp;mute=1&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;loop=1&amp;playlist=5h8FnQBrA-o" title="Pawthereum donates $25k to North Shore Animal League America" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className="rounded-xl" style={{aspectRatio: 16/9}} width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/5h8FnQBrA-o?rel=0&amp;autoplay=1&amp;mute=1&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;loop=1&amp;playlist=5h8FnQBrA-o" title="Pawthereum donates $25k to North Shore Animal League America" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
             </div>
           </div>
