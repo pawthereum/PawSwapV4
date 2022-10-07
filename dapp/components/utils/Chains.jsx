2985d84f3b47a7257aaa046603ea93797b674fea
Myk <89474773+mykcryptodev@users.noreply.github.com>
2022-10-06 20:38:33 -0400

    show selected chain

--- dapp/components/utils/Chains.jsx
+++ dapp/components/utils/Chains.jsx
@@ -2,6 +2,7 @@ import { validChains } from '../../constants';
 import { defaultChainId } from '../../constants';
 import { useState, useEffect } from 'react';
 import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
+import { CheckCircle } from 'react-feather';
 
 export const Chains = () => {
   const { isConnected } = useAccount();
@@ -19,7 +20,10 @@ export const Chains = () => {
 
   const chainLogo = {
     1: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
+    3: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
     4: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
+    5: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
+    42: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
     1337: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
     56: 'https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850',
     97: 'https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850',
@@ -33,15 +37,20 @@ export const Chains = () => {
         </label>
         <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-neutral-content text-neutral rounded-box w-52">
           { !validChains ? <></> : validChains
-            ?.map(chain => (
-              <li key={chain.id}>
+            ?.map(c => (
+              <li key={c.id}>
                 <a
                   className="hover:bg-neutral-focus hover:text-neutral-content"
                   onClick={async () => {
-                    switchNetwork?.(chain?.id);
+                    switchNetwork?.(c?.id);
                   }}
                 >
-                  {chain?.name}
+                  <img src={chainLogo[c?.id]} className="h-8 w-8" />
+                  <div className="flex justify-between items-center">
+                    <span>{c?.name}</span>
+                    { c.id === chain?.id ? <CheckCircle className="h-5 w-5 ml-1 text-success" /> : <></> }
+                  </div>
+
                 </a>
               </li>
           ))}
